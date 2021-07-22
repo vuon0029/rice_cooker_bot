11e068bec8c87ce0defad8ee161421363cce290e
Vuong Tuan Kiet <55266028+vuon0029@users.noreply.github.com>
2021-07-22 17:52:20 -0400

    added ping, github commands

---
+++ commands/github.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: "github",
+  description: "My body, my internals...",
+  execute(message, args) {
+    message.channel.send("https://github.com/vuon0029/rice_cooker_bot");
+  },
+};
--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 const Discord = require("discord.js");
 require("dotenv").config();
+const fs = require("fs");
 
 const client = new Discord.Client();
 
+const prefix = "/";
+
 client.once("ready", () => {
   console.log("Rice Cooker cookin!");
+});
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+
+  client.commands.set(command.name, command);
+}
+
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) {
+    return;
+  } else {
+    switch (message.content.toLowerCase()) {
+      case "/ping":
+        client.commands.get("ping").execute(message);
+        break;
+      case "/github":
+        client.commands.get("github").execute(message);
+        break;
+      default:
+        message.channel.send(`~~What the heck??~~ Try typing **/help**`);
+    }
+  }
 });
 
 client.login(process.env.TOKEN);
