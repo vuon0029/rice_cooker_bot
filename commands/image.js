5e7788660308bfb7ca64b7df18457b2dbcf34065
Vuong Tuan Kiet <55266028+vuon0029@users.noreply.github.com>
2021-07-23 00:43:56 -0400

    added args for puppeteer

--- commands/image.js
+++ commands/image.js
@@ -2,7 +2,8 @@ var Scraper = require("images-scraper");
 
 const google = new Scraper({
   puppeteer: {
-    headless: false,
+    headless: true,
+    args: ["--no-sandbox"],
   },
 });
 
@@ -16,20 +17,20 @@ module.exports = {
         return message.channel.send(
           "Image name not declared :( *sad rice cooker noises*"
         );
+      } else {
+        message.channel.send("*Cookin...*");
       }
-
       google
         .scrape(image_query, 1)
         .then((image_results) => {
-          message.channel.send("*Cookin...*");
-          message.channel.send(`So I found ${image_query}: `, {
+          message.channel.send(`So I found **${image_query}**: `, {
             files: [image_results[0].url],
             split: true,
           });
-          console.log(image_results[0].url);
         })
         .catch((err) => {
           message.channel.send(`Error: ${err}`);
+          console.log("Error: ", err);
         });
     } catch (err) {
       message.channel.send(`My papa wrote shitty code... I'm ded
